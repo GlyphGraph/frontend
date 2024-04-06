25d6b1c37b5f6fcddc158a5a58ea326645310f00
notRyuk <amadeusrobot.notryuk@gmail.com>
2024-04-06 19:30:22 +0530

    hello mereged error package

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import { NextUIProvider } from "@nextui-org/react";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.scss";
-import { BrowserRouter } from "react-router-dom";
 import WalletProvider from "./providers/WalletProvider.jsx";
-import { GoogleOAuthProvider } from "@react-oauth/google";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
