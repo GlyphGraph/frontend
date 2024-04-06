58c37f5d8e7ddbddbfd8c47540b3b573184ea797
notRyuk <amadeusrobot.notryuk@gmail.com>
2024-04-06 21:47:44 +0530

    Ui

--- src/pages/Register/index.jsx
+++ src/pages/Register/index.jsx
@@ -8,8 +8,14 @@ import { useAuth } from "@context/auth";
 import { Chip, Avatar } from "@nextui-org/react";
 import metamaskIcon from "@assets/metamask.webp";
 
+const Stages = Object.freeze({
+    Accounts: "accounts",
+    Password: "password"
+})
+
 export default function Signup() {
-    const { auth, connectToWallet, setAuth } = useAuth()
+    const [stage, setStage] = useState(Stages.Accounts)
+    const { auth, connectToWallet, setAuth, getProvider } = useAuth()
     const [, setUser] = useState()
     const [token, setToken] = useState()
     const [details, setDetails] = useState()
@@ -42,6 +48,16 @@ export default function Signup() {
         }
     }, [])
 
+    const handleRegister = async () => {
+        if(stage === Stages.Accounts) {
+            setStage(Stages.Password)
+            return
+        }
+        if(stage === Stages.Password) {
+            return
+        }
+    }
+
 
     return (
         <div className=''>
@@ -59,87 +75,130 @@ export default function Signup() {
                             </h2>
                         </div>
 
-                        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-                            <div className="flex justify-center">
-                                {auth && (
-                                    <Chip
-                                        color="warning"
-                                        variant="bordered"
-                                        avatar={
-                                            <Avatar src={metamaskIcon} />
-                                        }
-                                        size="lg"
-                                        onClick={() => {
-                                            setAuth(null);
-                                            connectToWallet()
-                                        }}
-                                        className="cursor-pointer"
-                                    >
-                                        {auth?.accountAddr.substr(0, 4) + "..." + auth?.accountAddr.substr(-4)}
-                                    </Chip>
-                                )}
-                            </div>
-                            <form className="space-y-6" action="#" method="POST">
-                                <div>
-                                    <label htmlFor="email" className="block text-sm font-medium leading-6 text-white">
-                                        Email address
-                                    </label>
-                                    <div className="mt-2">
-                                        <input
-                                            id="email"
-                                            name="email"
-                                            type="email"
-                                            autoComplete="email"
-                                            required
-                                            className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                                        />
-                                    </div>
+                        {stage === Stages.Accounts && (
+                            <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+                                <div className="flex justify-center">
+                                    {auth && (
+                                        <Chip
+                                            color="warning"
+                                            variant="bordered"
+                                            avatar={
+                                                <Avatar src={metamaskIcon} />
+                                            }
+                                            size="lg"
+                                            // onClick={async () => {
+                                            //     setAuth(null);
+                                            //     await getProvider();
+                                            //     await connectToWallet()
+                                            // }}
+                                            className="cursor-pointer"
+                                        >
+                                            {auth?.accountAddr.substr(0, 4) + "..." + auth?.accountAddr.substr(-4)}
+                                        </Chip>
+                                    )}
                                 </div>
-
-                                <div>
-                                    <div className="flex items-center justify-between">
-                                        <label htmlFor="password" className="block text-sm font-medium leading-6 text-white">
-                                            Password
+                                <form className="space-y-6" action="#" method="POST">
+                                    <div>
+                                        <label htmlFor="email" className="block text-sm font-medium leading-6 text-white">
+                                            Email address
                                         </label>
+                                        <div className="mt-2">
+                                            <input
+                                                id="email"
+                                                name="email"
+                                                type="email"
+                                                autoComplete="email"
+                                                required
+                                                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                            />
+                                        </div>
                                     </div>
-                                    <div className="mt-2">
-                                        <input
-                                            id="password"
-                                            name="password"
-                                            type="password"
-                                            autoComplete="current-password"
-                                            required
-                                            className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                                        />
-                                    </div>
-                                </div>
 
-                                <div>
+                                    <div>
+                                        <div className="flex items-center justify-between">
+                                            <label htmlFor="password" className="block text-sm font-medium leading-6 text-white">
+                                                Password
+                                            </label>
+                                        </div>
+                                        <div className="mt-2">
+                                            <input
+                                                id="password"
+                                                name="password"
+                                                type="password"
+                                                autoComplete="current-password"
+                                                required
+                                                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                            />
+                                        </div>
+                                    </div>
+
+                                    <div>
+                                        <button
+                                            type="submit"
+                                            className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                                            onClick={handleRegister}
+                                        >
+                                            Sign up
+                                        </button>
+                                    </div>
+                                </form>
+
+
+                                <div className=' flex items-center justify-center'>
                                     <button
-                                        type="submit"
-                                        className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                                        className={`flex mt-3 justify-center ${styles["login-with-google-btn"]}`}
+                                        onClick={() => login()}
                                     >
-                                        Sign up
+                                        Sign in with Google
                                     </button>
                                 </div>
-                            </form>
-
-
-                            <div className=' flex items-center justify-center'>
-                                <button
-                                    className={`flex mt-3 justify-center ${styles["login-with-google-btn"]}`}
-                                    onClick={() => login()}
-                                >
-                                    Sign in with Google
-                                </button>
+                                <p className="mt-10 text-center text-sm text-gray-500">
+                                    Already a member?{' '}
+                                    <a href="/login" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
+                                        Sign in
+                                    </a>
+                                </p>
                             </div>
-                            <p className="mt-10 text-center text-sm text-gray-500">
-                                Already a member?{' '}
-                                <a href="/login" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
-                                    Sign in
-                                </a>
-                            </p>
-                        </div>
+                        )}
+                        {stage === Stages.Password && (
+                            <div className="grid grid-rows-5 grid-cols-5">
+                                <div>
+                                    <div></div>
+                                    <div></div>
+                                    <div></div>
+                                    <div></div>
+                                    <div></div>
+                                </div>
+                                <div>
+                                    <div></div>
+                                    <div></div>
+                                    <div></div>
+                                    <div></div>
+                                    <div></div>
+                                </div>
+                                <div>
+                                    <div></div>
+                                    <div></div>
+                                    <div></div>
+                                    <div></div>
+                                    <div></div>
+                                </div>
+                                <div>
+                                    <div></div>
+                                    <div></div>
+                                    <div></div>
+                                    <div></div>
+                                    <div></div>
+                                </div>
+                                <div>
+                                    <div></div>
+                                    <div></div>
+                                    <div></div>
+                                    <div></div>
+                                    <div></div>
+                                </div>
+                            </div>
+                        )}
                     </div>
                 </div>
             </div>
